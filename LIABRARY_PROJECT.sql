affd1d394b44d7732c01e34db2186f1f5f2d4a85
SHAIKHSHAHI <shaikhshahi326@GMAIL.COM>
2024-11-25 08:40:33 +0530

    Update LIABRARY_PROJECT.sql

--- LIABRARY_PROJECT.sql
+++ LIABRARY_PROJECT.sql
@@ -1,4 +1,8 @@
-
+Library Management System using SQL Project --P2
+Project Overview
+Project Title: Library Management System
+Level: Intermediate
+Database: library_db
 SELECT * FROM return_status;
 SELECT * FROM issued_status;
 SELECT * FROM members;
