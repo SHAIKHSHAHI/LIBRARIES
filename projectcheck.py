aa45da9de1796f89ba8af8a8537979115e55ce2f
SHAIKHSHAHI <shaikhshahi326@GMAIL.COM>
2025-05-17 18:38:50 +0530

    Update projectcheck.py

--- projectcheck.py
+++ projectcheck.py
@@ -7,7 +7,11 @@ x=["eng","hindi","marathi"]
 y=[70,80,90]
 plt.plot(x,y,color="r")
 plt.title("Marks of Subject")
+plt.savefig("sales_plot.png")
 plt.show()
+### Sales Plot:
+
+![Sales Plot](sales_plot.png)
 
 
 x=["eng","hindi","marathi"]
