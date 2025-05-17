a58139750f55c9cb9e711765769bbb554833309a
SHAIKHSHAHI <shaikhshahi326@GMAIL.COM>
2025-05-17 18:37:14 +0530

    Add files via upload

---
+++ projectcheck.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x=["eng","hindi","marathi"]
+y=[70,80,90]
+plt.plot(x,y,color="r")
+plt.title("Marks of Subject")
+plt.show()
+
+
+x=["eng","hindi","marathi"]
+y=[70,80,90]
+plt.bar(x,y,color="r")
+plt.title("Marks of Subject")
+plt.show()
+
+
+
+
